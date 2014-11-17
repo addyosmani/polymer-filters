0cf9aa8f61ccfc3720de7a2d9d017c97c1b79205
Benjamin A. Petersen <bpetersen@bandwidth.com>
2014-11-17 11:43:09 -0500

    Add check to ensure truncate does not error
    - check if input exists before acting on it
    - silences thrown error
    	- spirit of template filter, do nothing rather than fail if input is incorrect

--- filter-truncate.js
+++ filter-truncate.js
@@ -1,15 +1,17 @@
 /**
  * Return a truncated version of a string
- * @param  {string} input     
- * @param  {integer} length   
- * @param  {boolean} killwords 
- * @param  {string} end       
- * @return {string}           
+ * @param  {string} input
+ * @param  {integer} length
+ * @param  {boolean} killwords
+ * @param  {string} end
+ * @return {string}
  */
 PolymerExpressions.prototype.truncate = function (input, length, killwords, end) {
     var orig = input;
     length = length || 255;
-
+    if(!input) {
+        return;
+    }
     if (input.length <= length)
         return input;
 
